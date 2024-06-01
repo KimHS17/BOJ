7d06352d058dba9a1850e64b70ae9897a56a592c
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-06-02 01:08:51 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {0, 0};
+        int[] answer = {0, sequence.length};
         int l = 0, r = 0;
         int sum = sequence[0];
 
@@ -24,11 +24,7 @@ class Solution {
                 }
             }
             else if(sum == k) {
-                if(answer[1] == 0) {
-                    answer[0] = l;
-                    answer[1] = r;
-                }
-                else if(answer[1] - answer[0] > r - l) {
+                if(answer[1] - answer[0] > r - l) {
                     answer[0] = l;
                     answer[1] = r;
                 }
