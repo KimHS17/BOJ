44156bde098adfc5a01fe940bf69f2a9ef68d9d8
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-07-09 23:00:05 +0900

    [level 2] Title: 호텔 대실, Time: 844.26 ms, Memory: 405 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -9,17 +9,19 @@ class Solution {
     }
     
     public int solution(String[][] book_time) {
-        ArrayList room = new ArrayList();
+        ArrayList<String> room = new ArrayList();
 
         Arrays.sort(book_time, (a, b) -> {
             return time(a[0]).compareTo(time(b[0]));
         });
         
-        room.add(time(book_time[0][1]));
+        room.add(book_time[0][1]);
         for(int i = 1; i < book_time.length; i++) {
             boolean isAfter = false;
+            
             for(int j = 0; j < room.size(); j++) {
-                LocalTime endTime = time(room.get(j).toString());
+                LocalTime endTime = time(room.get(j));
+                
                 if(endTime.isAfter(time("23:49"))) {
                     isAfter = true;
                     continue;
@@ -27,17 +29,18 @@ class Solution {
                 else {
                     endTime = endTime.plusMinutes(10);
                 }
+                
                 if(endTime.isAfter(time(book_time[i][0]))) {
                     isAfter = true;
                 }
                 else {
                     isAfter = false;
-                    room.set(j, time(book_time[i][1]));
+                    room.set(j, book_time[i][1]);
                     break;
                 }
             }
             if(isAfter) {
-                room.add(time(book_time[i][1]));
+                room.add(book_time[i][1]);
             }
         }
         
