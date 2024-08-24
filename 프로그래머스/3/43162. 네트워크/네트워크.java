c01ed0306cd3ad701b05608230b694c04d9a38d7
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-08-25 00:00:22 +0900

    [level 3] Title: 네트워크, Time: 1.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,20 +1,16 @@
 import java.util.*;
 
 class Solution {
-    static int ans = 0;
-    
     public static void bfs(int [][] computers, int n, int i) {
         Queue<Integer> q = new LinkedList<>();
         
         q.add(i);
-        computers[i][i] = 0;
-        ans++;
         
         while(!q.isEmpty()) {
             int now = q.poll();
             
             for(int k = 0; k < n; k++) {
-                if(computers[now][k] == 1 && computers[k][k] != 0) {
+                if(computers[now][k] == 1 && computers[k][k] != 0 && now != k) {
                     q.add(k);
                 }
                 computers[now][k] = 0;
@@ -23,14 +19,15 @@ class Solution {
     }
     
     public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(computers[i][j] == 1) {
-                    bfs(computers, n, i);
-                }
+            if(computers[i][i] == 1) {
+                bfs(computers, n, i);
+                answer++;
             }
         }
         
-        return ans;
+        return answer;
     }
 }
