5c903ff8676c00b428937aae0d6024cef8aed197
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-08-29 11:14:44 +0900

    [level 3] Title: 야근 지수, Time: 63.06 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        int id = works.length - 1;
+        
+        Arrays.sort(works);
+        for(int i = 0; i < n; i++) {
+            while(id > 0 && works[id] < works[id - 1]) {
+                id--;
+            }
+            while(id < works.length - 1 && works[id + 1] >= works[id]) {
+                id++;
+            }
+            
+            if(works[id] > 0) {
+                works[id]--;
+            }
+        }
+        
+        for(long w: works) {
+            System.out.print(w + " ");
+            answer += w * w;
+        }
+        
+        return answer;
+    }
+}
