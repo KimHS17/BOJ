2a9433cfdbdf798ec61cfe2e243d9691a2b9ce7c
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-08-29 17:41:18 +0900

    [level 3] Title: 숫자 게임, Time: 111.83 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -3,18 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-        int AId = A.length - 1, BId = A.length - 1;
+        int id = A.length - 1;
         
         Arrays.sort(A);
         Arrays.sort(B);
         
-        while(AId >= 0 && BId >= 0) {
-            if(A[AId] < B[BId]) {
+        for(int i = A.length - 1; i >=0; i--) {
+            if(A[i] < B[id]) {
                 answer++;
-                AId--;
-                BId--;
-            } else {
-                AId--;
+                id--;
             }
         }
         
