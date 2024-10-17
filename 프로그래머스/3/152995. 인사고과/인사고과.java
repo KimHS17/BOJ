1293ea1dcb6ba6681d4dd89acfeb8f975d083a54
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-10-17 18:06:25 +0900

    [level 3] Title: 인사고과, Time: 114.95 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -3,18 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int[][] scores) {
         int answer = 1;
-        int[] wh = scores[0];
-        int max = 0;
+        int[] s1 = new int[2];
+        int[] s2 = new int[2];
+        ArrayList<Integer> rank = new ArrayList<>();
         
-        Arrays.sort(scores, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         for(int[] score: scores) {
-            if(max <= score[1]) {
-                max = score[1];
-                if(score[0] + score[1] > wh[0] + wh[1]) {
-                    answer++;
+            if(score[0] > s1[0] || (score[0] == s1[0] && score[1] > s1[1])) {
+                s1[0] = score[0];
+                s1[1] = score[1];
+            }
+            if(score[1] > s2[1] || (score[1] == s2[1] && score[0] > s2[0])) {
+                s2[0] = score[0];
+                s2[1] = score[1];
+            }
+        }
+        
+        for(int i = 0; i < scores.length; i++) {
+            if((scores[i][0] < s1[0] && scores[i][1] < s1[1]) || (scores[i][0] < s2[0] && scores[i][1] < s2[1])) {
+                if(i == 0) {
+                    return -1;
                 }
-            } else if(score.equals(wh)) {
-                return -1;
+            } else if(scores[0][0] + scores[0][1] < scores[i][0] + scores[i][1]) {
+                answer++;
             }
         }
         
