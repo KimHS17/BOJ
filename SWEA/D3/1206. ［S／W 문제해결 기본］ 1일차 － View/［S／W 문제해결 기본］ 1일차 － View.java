4d1ed1b63add38b87d9a9e01427396c96e2fabf0
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-11-09 22:40:59 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 179 ms, Memory: 37,020 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -7,26 +7,26 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = 10;
-
+		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int n = sc.nextInt();
-            int[] building = new int[n + 4];
-            int cnt = 0;
-            
-            for(int i = 2; i < n + 2; i++) {
-                building[i] = sc.nextInt();
-            }
-            
-            for(int i = 2; i < n + 2; i++) {
-            	int left = Math.max(building[i - 1], building[i - 2]);
-                int right = Math.max(building[i + 1], building[i + 2]);
-                int h = Math.max(left, right);
-                
-                cnt += Math.max(building[i] - h, 0);
-            }
-            
-            System.out.println("#" + test_case + " " + cnt);
+			int[] building = new int[n + 4];
+			int cnt = 0;
+			
+			for(int i = 2; i < n + 2; i++) {
+				building[i] = sc.nextInt();
+			}
+			
+			for(int i = 2; i < n + 2; i++) {
+				int left = Math.max(building[i - 1], building[i - 2]);
+				int right = Math.max(building[i + 1], building[i + 2]);
+				int h = Math.max(left, right);
+				
+				cnt += Math.max(building[i] - h, 0);
+			}
+			
+			System.out.println("#" + test_case + " " + cnt);
 		}
 	}
 }
