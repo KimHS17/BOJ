a45bd2209ab6d3c2460db750beede1a8f58d0318
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-11-21 14:06:45 +0900

    [level 3] Title: 블록 이동하기, Time: 19.31 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/README.md
+++ 프로그래머스/3/60063. 블록 이동하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 블록 이동하기 - 60063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063#) 
 
 ### 성능 요약
 
-메모리: 84.3 MB, 시간: 18.66 ms
+메모리: 76.7 MB, 시간: 19.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 21일 13:37:58
+2024년 11월 21일 14:06:43
 
 ### 문제 설명
 
--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -30,22 +30,20 @@ class Solution {
                 int nx = now[1] + dx[i];
                 int sy = ny + ds[now[2]][0];
                 int sx = nx + ds[now[2]][1];
+                int ncnt = cnt[now[0]][now[1]][now[2]] + 1;
                 
-                if(ny < 0 || nx < 0 || ny >= n || nx >= n || board[ny][nx] == 1 || (visited[ny][nx][now[2]] && cnt[ny][nx][now[2]] <= cnt[now[0]][now[1]][now[2]] + 1))
+                if(ny < 0 || nx < 0 || ny >= n || nx >= n || board[ny][nx] == 1 || (visited[ny][nx][now[2]] && cnt[ny][nx][now[2]] <= ncnt))
                     continue;
                 
                 if(sy >= n || sx >= n || board[sy][sx] == 1)
                     continue;
                 
-                if(sy == n - 1 && sx == n - 1) {
-                    cnt[ny][nx][now[2]] = cnt[now[0]][now[1]][now[2]] + 1;
-                    answer = Math.min(answer, cnt[ny][nx][now[2]]);
-                    continue;
-                }
-                
                 q.add(new int[]{ny, nx, now[2]});
                 visited[ny][nx][now[2]] = true;
-                cnt[ny][nx][now[2]] = cnt[now[0]][now[1]][now[2]] + 1;
+                cnt[ny][nx][now[2]] = ncnt;
+                
+                if(sy == n - 1 && sx == n - 1)
+                    answer = Math.min(answer, ncnt);
             }
             
             for(int i = 0; i < 4; i++) {
@@ -56,6 +54,7 @@ class Solution {
                 int oy = ny + rsy[i];
                 int ox = nx + rsx[i];
                 int dir = now[2] == 0 ? 1 : 0;
+                int ncnt = cnt[ny][nx][now[2]] + 1;
                 
                 if(i == 1)
                     nx++;
@@ -65,18 +64,15 @@ class Solution {
                 if(now[2] == 0 && i > 1 || now[2] == 1 && i < 2)
                     continue;
                 
-                if(sy >= n || sx >= n || board[oy][ox] == 1 || board[sy][sx] == 1 || (visited[ny][nx][dir] && cnt[ny][nx][dir] <= cnt[now[0]][now[1]][now[2]] + 1))
+                if(sy >= n || sx >= n || board[oy][ox] == 1 || board[sy][sx] == 1 || (visited[ny][nx][dir] && cnt[ny][nx][dir] <= ncnt))
                     continue;
-                
-                if(sy == n - 1 && sx == n - 1) {
-                    cnt[ny][nx][dir] = cnt[now[0]][now[1]][now[2]] + 1;
-                    answer = Math.min(answer, cnt[ny][nx][dir]);
-                    continue;
-                }
                 
                 q.add(new int[]{ny, nx, dir});
                 visited[ny][nx][dir] = true;
-                cnt[ny][nx][dir] = cnt[now[0]][now[1]][now[2]] + 1;
+                cnt[ny][nx][dir] = ncnt;
+                
+                if(sy == n - 1 && sx == n - 1)
+                    answer = Math.min(answer, ncnt);
             }
             
             for(int i = 0; i < 4; i++) {
@@ -87,6 +83,7 @@ class Solution {
                 int oy = ny + rsy2[i];
                 int ox = nx + rsx2[i];
                 int dir = now[2] == 0 ? 1 : 0;
+                int ncnt = cnt[ny][nx][now[2]] + 1;
                 
                 if(i == 1)
                     nx++;
@@ -96,18 +93,15 @@ class Solution {
                 if(now[2] == 0 && i > 1 || now[2] == 1 && i < 2)
                     continue;
                 
-                if(sy < 0 || sx < 0 || board[oy][ox] == 1 || board[sy][sx] == 1 || (visited[sy][sx][dir] && cnt[sy][sx][dir] <= cnt[now[0]][now[1]][now[2]] + 1))
+                if(sy < 0 || sx < 0 || board[oy][ox] == 1 || board[sy][sx] == 1 || (visited[sy][sx][dir] && cnt[sy][sx][dir] <= ncnt))
                     continue;
-                
-                if(ny == n - 1 && nx == n - 1) {
-                    cnt[sy][sx][dir] = cnt[now[0]][now[1]][now[2]] + 1;
-                    answer = Math.min(answer, cnt[sy][sx][dir]);
-                    continue;
-                }
                 
                 q.add(new int[]{sy, sx, dir});
                 visited[sy][sx][dir] = true;
-                cnt[sy][sx][dir] = cnt[now[0]][now[1]][now[2]] + 1;
+                cnt[sy][sx][dir] = ncnt;
+                
+                if(ny == n - 1 && nx == n - 1)
+                    answer = Math.min(answer, ncnt);
             }
         }
     }
@@ -116,9 +110,6 @@ class Solution {
         n = board.length;
         visited = new boolean[n][n][2];
         cnt = new int[n][n][2];
-
-        cnt[n - 1][n - 1][0] = Integer.MAX_VALUE;
-        cnt[n - 1][n - 1][1] = Integer.MAX_VALUE;
         
         bfs(board);
         
